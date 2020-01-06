388596191316e3ad85dd2fd883d5f4143f4c8607
rnaster <blpomspo@gmail.com>
2020-01-06 19:26:57 +0900

    .

--- myrequest.py
+++ myrequest.py
@@ -17,25 +17,30 @@ def send_request(endpoint, body, methods='POST'):
 if __name__ == "__main__":
     print(send_request('check', {}, 'GET').ok)
     print(send_request('sign-up',
-                       {'name': 'lee', 'hobby': 'basketball'}).json())
+                       {'user_name': 'lee',
+                        'myprofile': 'i like League of Legend',
+                        'email': 'lee@never.com',
+                        'hashed_password': 'lol'}).json())
     print(send_request('sign-up',
-                       {'name': 'kim', 'hobby': 'soccer'}).json())
+                       {'user_name': 'kim',
+                        'myprofile': 'i`m studying Flask in Python. it`s very boring',
+                        'email': 'kim@never.com',
+                        'hashed_password': 'what'}).json())
     print(send_request('sign-up',
-                       {'name': 'park', 'hobby': 'watching movie'}).json())
+                       {'user_name': 'park',
+                        'myprofile': 'i love basketball',
+                        'email': 'park@never.com',
+                        'hashed_password': 'ball'}).json())
     print(send_request('tweet',
-                        {'id': 0, 'tweet': 'i`m tired.'}).json())
+                        {'user_id': 1, 'tweet': 'i`m tired.2'}).content)
     print(send_request('tweet',
-                        {'id': 2, 'tweet': 'i`m happy.'}).json())
+                        {'user_id': 2, 'tweet': 'i`m happy.2'}).text)
     print(send_request('tweet',
-                       {'id': 1, 'tweet': 'i`m hungry.'}).json())
+                       {'user_id': 3, 'tweet': 'i`m hungry.2'}).status_code)
     print(send_request('tweet',
-                       {'id': 1, 'tweet': 'i`m thirsty.'}).json())
+                       {'user_id': 2, 'tweet': 'i`m thirsty.2'}))
     print(send_request('follow',
-                       {'id': 0, 'follow': 1}).ok)
+                       {'user_id': 1, 'user_id_following': 2}))
     print(send_request('follow',
-                       {'id': 1, 'follow': 0}).ok)
-    print(send_request('unfollow',
-                       {'id': 1, 'follow': 0}).ok)
-    print(send_request('follow',
-                       {'id': 0, 'follow': 2}).ok)
-    print(send_request('timeline/%s' % 0, {}, 'GET').json())
+                       {'user_id': 1, 'user_id_following': 4}))
+    print(send_request('timeline/2', {}, 'GET').json())
