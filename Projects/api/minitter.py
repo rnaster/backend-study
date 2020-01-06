9f318f3b04f7e33cd247de9f78ec171fdd30ab51
rnaster <blpomspo@gmail.com>
2020-01-06 19:26:12 +0900

    minitter with MySQL

--- Projects/api/minitter.py
+++ Projects/api/minitter.py
@@ -1,92 +1,125 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-app.users = []
-app.user_id_cnt = 0
-app.tweets = []
-app.tweets_id_cnt = 0
-app.follow = {}
-
-
-@app.route('/check', methods=['GET'])
-def check():
-    return '', 202
-
-
-@app.route('/sign-up', methods=['POST'])
-def sign_up():
-    new_user = request.json
-    new_user['id'] = app.user_id_cnt
-    app.users.append(new_user)
-    app.user_id_cnt += 1
-    return jsonify(new_user)
+from flask import Flask, jsonify, request, current_app
+from sqlalchemy import create_engine, text
 
 
 def is_user(user_id):
-    for user in app.users:
+    for user in current_app.users:
         if user['id'] == user_id:
             return True
     return False
 
 
-@app.route('/tweet', methods=['POST'])
-def tweet():
-    """
-    less then 300 chars
-    """
-    data = request.json
-    user_id = int(data.get('id', -1))
-    text = data.get('tweet', '')
-    if not is_user(user_id):
-        return 'Wrong user id', 400
-    if len(text) > 300:
-        return 'Exceed 300 chars', 400
-    app.tweets.append({
-        '_id': app.tweets_id_cnt,
-        'user_id': user_id,
-        'tweet': text
-    })
-    app.tweets_id_cnt += 1
-    return jsonify(app.tweets[-1])
+def insert_user(user):
+    return current_app.db.execute(text("""
+            INSERT INTO users (
+                user_name,
+                email,
+                myprofile,
+                hashed_password
+            ) VALUES (
+                :user_name,
+                :email,
+                :myprofile,
+                :hashed_password
+            )
+        """), user)
 
 
-@app.route('/follow', methods=['POST'])
-def follow():
-    data = request.json
-    source = int(data.get('id', 0))
-    target = int(data.get('follow', 0))
-    if not is_user(source) or not is_user(target):
-        return 'Wrong user id', 400
-    app.follow.setdefault(source, {target}).add(target)
-    print(app.follow)
-    return '', 202
+def insert_tweet(message):
+    return current_app.db.execute(text(
+        """
+        insert into tweets (
+            user_id,
+            tweet
+        ) values (
+            :user_id,
+            :tweet
+        )
+        """
+    ), message)
 
 
-@app.route('/unfollow', methods=['POST'])
-def unfollow():
-    data = request.json
-    source = int(data.get('id', 0))
-    target = int(data.get('follow', 0))
-    if not is_user(source) \
-            or not is_user(target) \
-            or target not in app.follow.get(source, set()):
-        return 'Wrong user id', 400
-    app.follow[source].remove(target)
-    print(app.follow)
-    return '', 202
+def get_follow(follow):
+    cursor = current_app.db.execute(text(
+        """
+        select isfollowing
+        from users_following
+        where user_id = :user_id and
+              user_id_following = :user_id_following
+        """
+    ), follow)
+    res = cursor.fetchone()
+    if res:
+        return {'isfollowing': 1-res[0]}
+    return {}
 
 
-@app.route('/timeline/<int:user_id>', methods=['GET'])
-def timeline(user_id):
-    my_timeline = []
-    for f_user in app.follow.get(user_id, set()):
-        last_tweet = {'user_id': f_user, 'tweet': ''}
-        for tweet in app.tweets:
-            if tweet['user_id'] == f_user:
-                last_tweet = tweet
-        my_timeline.append(last_tweet)
-    return jsonify(my_timeline)
+def upsort_follow(follow):
+    isfollowing = get_follow(follow)
+    follow.update(isfollowing)
+    return current_app.db.execute(text(
+        """
+        insert into users_following (
+        user_id,
+        user_id_following
+        ) values (
+        :user_id,
+        :user_id_following
+        )
+        on duplicate key update isfollowing = :isfollowing 
+        """
+    ), follow)
+
+
+def run_app(config=None):
+    app = Flask(__name__)
+    if config is None:
+        app.config.from_json('test_config.json')
+    else:
+        app.config.update(config)
+    app.db = create_engine(app.config['DB_URL'], encoding='utf-8')
+
+    @app.route('/check', methods=['GET'])
+    def check():
+        return '', 202
+
+    @app.route('/sign-up', methods=['POST'])
+    def sign_up():
+        new_user = request.json
+        insert_user(new_user)
+        return jsonify(new_user)
+
+    @app.route('/tweet', methods=['POST'])
+    def tweet():
+        """
+        less then 300 chars
+        """
+        message = request.json
+        if len(message.get('tweet', '')) > 300:
+            return 'Exceed 300 chars', 400
+        insert_tweet(message)
+        return jsonify(message)
+
+    @app.route('/follow', methods=['POST'])
+    def follow():
+        data = request.json
+        upsort_follow(data)
+        return '', 202
+
+    @app.route('/timeline/<int:user_id>', methods=['GET'])
+    def timeline(user_id):
+        cursor = current_app.db.execute(text(
+            """
+            select _id, tweet
+            from tweets
+            where user_id = :user_id
+            """
+        ), {'user_id': int(user_id)})
+        tweets = cursor.fetchall()
+        return dict(tweets)
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    run_app().run(debug=True)
